814eae94e6649d9971dfe627f3734b9a73a9dada
alfaizuna <alfaizunaulia@gmail.com>
2024-09-04 20:15:25 +0700

    Initial Commit

---
+++ BackgroundTimer/BackgroundTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundTimerApp.swift
+//  BackgroundTimer
+//
+//  Created by Alfaizuna Aulia Ulloh on 04/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
